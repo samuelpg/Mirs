460481a2e6e9d157b0b0126d695adc5427c5a355
Samuel Parra <samuelparra1@gmail.com>
2017-11-01 21:25:42 -0400

    3rd commit, modifications

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,23 @@
 $( document ).ready(function(){
     $(".button-collapse").sideNav();
+
+    //contacto    
+    var $name = $("#NombreyApellido");
+    var $email = $("#email");
+    var $cellphone = $("#Telefono");
+    var $empresa = $("#Empresa");
+    var $message = $("#Mensaje");
+    
+    $("#sendMail").click(function(){
+        console.log("AAA")
+        if(($name.val()=="") || ($email.val()=="") || ($cellphone.val()=="") || ($empresa.val()=="") || ($message.val()=="")){
+            $(".hideb-2").addClass("show");
+        }else{
+            $(".hideb-2").removeClass("show");
+            $(".hideb").addClass("show");
+            $("#sendMail").addClass("disabled");
+        }
+    });
 })
+
+
